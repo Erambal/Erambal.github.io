7310a237610f0b3393e229725f89be8fa97966d2
Your Name <ram14018@byui.edu>
2019-07-14 23:37:27 -0600

    project update

--- finalproject/acme/js/acme.js
+++ finalproject/acme/js/acme.js
@@ -1,40 +1,70 @@
 let Nav = document.getElementById('nav');
+let acme = "https://Erambal.github.io/finalproject/acme/js/acme.json";
 
 Nav.addEventListener('click', function(evt){
-    // Get the city name
-    let name = evt.target.innerHTML;
-    switch (name) {
-      case "Anvil":
-        case "Explosives":
-          case "Decoys":
-              case "Traps":
-            console.log(name);
-            evt.preventDefault();
-        break;
-    }
-})
+
+   // Get the city name
+   let name = evt.target.innerHTML;
+   switch (name) {
+     case "Anvil":
+       case "Explosives":
+         case "Decoys":
+             case "Traps":
+           console.log(name);
+           evt.preventDefault();
+       break;
+   }
+   fetch(acme)
+   .then(function(response) {
+     if(response.ok){
+       return response.json();
+     }
+     throw new Error("Network response was no bueno.");
+   }) 
+   .then(function(data){
+       let g = data[name];
+
+       let name1 = g.name;
+       console.log(name1);
+       let path = g.path;
+       let description = g.description;
+       let manufacturer = g.manufacturer;
+       let price = g.price;
+       let reviews = g.reviews;
+   })
+   .catch(function(error){
+     console.log("There was a fetching problem: ", error.message);
+   })
+ })
 
 
 function nav(){
-  let nav = "<li>Home</li>";
-  let acme = "../finalproject\acme\js\acme.json"
-  
-  fetch(acme)
-    .then(function(response) {
-      if(response.ok){
-        return response.json();
-      }
-      throw new Error("Network response was no bueno.");
-    })
-    .then(function(data){
-      console.log(data);
+ let nav = "<li>Home</li>";
+
+ fetch(acme)
+   .then(function(response) {
+     if(response.ok){
+       return response.json();
+     }
+     throw new Error("Network response was no bueno.");
+   })
+   .then(function(data){
+     console.log(data);
 //Create array with all of the different keys
 let product = Object.keys(data);
-     console.log(product);
-    })
-    .catch(function(error){
-      console.log("There was a fetching problem: ", error.message);
-    })
+    console.log(product);
+
+    for (let i = 0, x = product.length; i < x; i++){
+      nav += "<li>" + product[i] + "</li>";
+    }
+    document.getElementById("links").innerHTML = nav;
+
+   })
+   .catch(function(error){
+     console.log("There was a fetching problem: ", error.message);
+   })
 }
 nav();
-      
+    
+
+
